1f4986ba2a9ce5b03349782d13a713019c9e0d2e
Alex  Douglas <alex.almeida@alfaerp.com.br>
2025-05-04 23:31:47 +0200

    Implementing grouping search .

--- Ponto&VirgulaSalesWeb/Models/SalesRecord.cs
+++ Ponto&VirgulaSalesWeb/Models/SalesRecord.cs
@@ -1,11 +1,16 @@
 ﻿using Ponto_VirgulaSalesWeb.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Ponto_VirgulaSalesWeb.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller  { get; set; }
--- Ponto&VirgulaSalesWeb/Services/SalesRecordService.cs
+++ Ponto&VirgulaSalesWeb/Services/SalesRecordService.cs
@@ -10,11 +10,11 @@ namespace Ponto_VirgulaSalesWeb.Services
         public SalesRecordService(Ponto_VirgulaSalesWebContext context)
         {
             _context = context;
-        }       
-        
+        }
+
         public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
         {
-            var result = from obj in  _context.SalesRecords select obj;
+            var result = from obj in _context.SalesRecords select obj;
             if (minDate.HasValue)
             {
                 result = result.Where(x => x.Date >= minDate.Value);
@@ -28,6 +28,37 @@ namespace Ponto_VirgulaSalesWeb.Services
                 .Include(x => x.Seller.Department)
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
-        }   
+        }
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecords select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            //Dessa forma estava dando erro uma exceção 
+            
+            //return await result
+            //    .Include(x => x.Seller)
+            //    .Include(x => x.Seller.Department)
+            //    .OrderByDescending(x => x.Date)
+            //    .GroupBy(x => x.Seller.Department)
+            //    .ToListAsync();
+
+             var list = await result
+            .Include(x => x.Seller)
+            .ThenInclude(s => s.Department)
+            .OrderByDescending(x => x.Date)
+            .ToListAsync(); // executa a query no banco
+
+            return list
+                .GroupBy(x => x.Seller.Department) // faz o agrupamento na memória
+                .ToList();
+        }
     }
 }
