8fe8034c3f1babf41934bd417de6568bff94e59d
Alex  Douglas <alex.almeida@alfaerp.com.br>
2025-05-04 22:15:20 +0200

    Implementing simple search.

---
+++ Ponto&VirgulaSalesWeb/Services/SalesRecordService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ponto_VirgulaSalesWeb.Data;
+using Ponto_VirgulaSalesWeb.Models;
+
+namespace Ponto_VirgulaSalesWeb.Services
+{
+    public class SalesRecordService
+    {
+        private readonly Ponto_VirgulaSalesWebContext _context;
+        public SalesRecordService(Ponto_VirgulaSalesWebContext context)
+        {
+            _context = context;
+        }       
+        
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in  _context.SalesRecords select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }   
+    }
+}
